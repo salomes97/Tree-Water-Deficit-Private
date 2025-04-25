857c5c7caa17b54a243266db50f75ebd00bb12cb
Salome Santos <sromeiro@ethz.ch>
2025-04-25 08:08:43 +0200

    Add files via upload
    add map in black and white

--- app.r
+++ app.r
@@ -9,6 +9,28 @@ df_twd <- read.csv("df_twd.csv")
 # Ensure the 'ts' column is of Date type
 df_twd$ts <- as.Date(df_twd$ts)
 
+num_cols <- c("twd", "pr", "at", "ws", "dp", "sr", "lr")
+species <- unique(df_twd$species)
+species <- c("All", species)
+
+df_twd <- df_twd %>%
+  group_by(site, year, month) %>%
+  mutate(
+    across(
+      all_of(num_cols),
+      ~ mean(.x, na.rm = TRUE),
+      .names = "{col}_avg"
+    )
+  ) %>%
+  group_by(site, species, week, year) %>%
+  mutate(
+    across(
+      all_of(num_cols),
+      ~ mean(.x, na.rm = TRUE),
+      .names = "{col}_month_avg"
+    )
+  )
+
 # Define UI for dataset viewer app ----
 ui <- fluidPage(
   
@@ -23,8 +45,11 @@ ui <- fluidPage(
       
       # Dropdown menu for column selection (multiple selections allowed)
       selectInput("columns", "Select Columns to Display:",
-                  choices = names(df_twd)[!(names(df_twd) %in% c("site_longitude", "site_latitude", "ts"))],  # Exclude longitude, latitude, and date columns
-                  selected = "twd", multiple = TRUE),  # Default selection is "twd"
+                  choices = num_cols,
+                  selected = "twd", multiple = TRUE),
+      selectInput("species", "Select Species:",
+                  choices = species,
+                  selected = "All"),
       
       # Slider for date selection
       sliderInput("date", "Select Date:",
@@ -45,7 +70,12 @@ ui <- fluidPage(
       # Slider for month selection
       sliderInput("month", "Select Month:",
                   min = 1, max = 12,
+                  value = 1, animate = TRUE),
+
+      sliderInput("week", "Select Week:",
+                  min = 1, max = 52,
                   value = 1, animate = TRUE)
+
     ),
     
     # Main panel for displaying outputs ----
@@ -64,6 +94,10 @@ ui <- fluidPage(
         # Map tab based on Year and Month ----
         tabPanel("Map by Year and Month",
                  uiOutput("dynamic_maps_year_month")
+        ),
+
+        tabPanel("Map by Year and Week",
+                 uiOutput("dynamic_maps_year_week")
         )
       )
     )
@@ -72,17 +106,36 @@ ui <- fluidPage(
 
 # Define server logic to summarize and view selected dataset ----
 server <- function(input, output) {
+
   
   # Return the requested dataset based on the selected date ----
   datasetInput <- reactive({
-    df_twd %>%
+    df_twd <- df_twd %>%
       filter(ts == input$date)
+    return(df_twd)
   })
   
   # Return the dataset based on the selected year and month ----
   datasetYearMonthInput <- reactive({
-    df_twd %>%
+    df_twd <- df_twd %>%
       filter(year(ts) == input$year, month(ts) == input$month)
+    
+    if(input$species != "All") {
+      df_twd <- df_twd %>%
+        filter(species == input$species)
+    }
+    return(df_twd)
+  })
+
+  datasetYearWeekInput <- reactive({
+    df_twd <- df_twd %>%
+      filter(year(ts) == input$year, week(ts) == input$week)
+    
+    if(input$species != "All") {
+      df_twd <- df_twd %>%
+        filter(species == input$species)
+    }
+    return(df_twd)
   })
   
   # Generate a summary of the dataset ----
@@ -92,23 +145,26 @@ server <- function(input, output) {
   
   # Show the first "n" observations ----
   output$view <- renderTable({
-    head(datasetInput(), n = 10)
+    head(datasetYearMonthInput(), n = 10)
   })
   
   # Dynamically generate maps based on selected columns and Date ----
   output$dynamic_maps_date <- renderUI({
     req(input$columns)  # Ensure at least one column is selected
-    
+    cols_to_show <- input$columns
+
     map_outputs <- lapply(input$columns, function(col) {
       renderLeaflet({
-        data <- datasetInput()
-        
+        data <- datasetYearMonthInput()
+
         # Create a color palette based on the selected column
         color_pal <- colorNumeric(palette = c("green", "yellow", "red"), domain = data[[col]])
         
         # Create the map with markers
         leaflet(data) %>%
           addTiles() %>%
+          addProviderTiles("Esri.WorldGrayCanvas") %>% 
+          #addProviderTiles(providers$Stamen.Toner) %>% 
           addCircleMarkers(
             lng = ~site_longitude, lat = ~site_latitude,
             radius = 8, color = ~color_pal(data[[col]]),
@@ -133,27 +189,64 @@ server <- function(input, output) {
     req(input$columns)  # Ensure at least one column is selected
     
     map_outputs <- lapply(input$columns, function(col) {
+      avg_col <- paste0(col, "_avg")
       renderLeaflet({
         data <- datasetYearMonthInput()
         
         # Create a color palette based on the selected column
-        color_pal <- colorNumeric(palette = c("green", "yellow", "red"), domain = data[[col]])
+        color_pal <- colorNumeric(palette = c("green", "yellow", "red"), domain =  df_twd[[col]])
         
         # Create the map with markers
         leaflet(data) %>%
           addTiles() %>%
+          addProviderTiles("Esri.WorldGrayCanvas") %>% 
           addCircleMarkers(
             lng = ~site_longitude, lat = ~site_latitude,
-            radius = 8, color = ~color_pal(data[[col]]),
+            radius = 8, color = ~color_pal(data[[avg_col]]),
             fill = TRUE, fillOpacity = 0.7,
             popup = ~paste(
               "Site: ", site, "<br>",
               "Species: ", species, "<br>",
-              paste(col, ": ", round(data[[col]], 2)), "<br>",
+              paste(avg_col, ": ", round(data[[avg_col]], 2)), "<br>",
               "Date: ", ts
             )
           ) %>%
-          addLegend("bottomright", pal = color_pal, values = ~data[[col]], title = col, opacity = 1)
+          addLegend("bottomright", pal = color_pal, values = ~data[[avg_col]], title = avg_col, opacity = 1)
+      })
+    })
+    
+    # Return the list of dynamic maps
+    do.call(tagList, map_outputs)
+  })
+
+
+  output$dynamic_maps_year_week <- renderUI({
+    req(input$columns)  # Ensure at least one column is selected
+    
+    map_outputs <- lapply(input$columns, function(col) {
+      avg_col <- paste0(col, "_month_avg")
+      renderLeaflet({
+        data <- datasetYearWeekInput()
+        
+        # Create a color palette based on the selected column
+        color_pal <- colorNumeric(palette = c("green", "yellow", "red"), domain =  df_twd[[col]])
+        
+        # Create the map with markers
+        leaflet(data) %>%
+          addTiles() %>%
+          addProviderTiles("Esri.WorldGrayCanvas") %>% 
+          addCircleMarkers(
+            lng = ~site_longitude, lat = ~site_latitude,
+            radius = 8, color = ~color_pal(data[[avg_col]]),
+            fill = TRUE, fillOpacity = 0.7,
+            popup = ~paste(
+              "Site: ", site, "<br>",
+              "Species: ", species, "<br>",
+              paste(avg_col, ": ", round(data[[avg_col]], 2)), "<br>",
+              "Date: ", ts
+            )
+          ) %>%
+          addLegend("bottomright", pal = color_pal, values = ~data[[avg_col]], title = avg_col, opacity = 1)
       })
     })
     
@@ -162,5 +255,4 @@ server <- function(input, output) {
   })
 }
 
-# Create Shiny app ----
 shinyApp(ui, server)
